484a143528818d4d21c13784871ddf0833c5b4f1
Hchanjune <hchanjune@gmail.com>
2023-11-10 18:32:18 +0900

    update

--- src/main/kotlin/com/kotlin/spring/management/configurations/security/CustomAuthenticationProvider.kt
+++ src/main/kotlin/com/kotlin/spring/management/configurations/security/CustomAuthenticationProvider.kt
@@ -1,20 +1,28 @@
 package com.kotlin.spring.management.configurations.security
 
 import org.springframework.security.authentication.AuthenticationProvider
+import org.springframework.security.authentication.BadCredentialsException
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken
 import org.springframework.security.core.Authentication
 import org.springframework.security.core.userdetails.UserDetails
+import org.springframework.security.crypto.password.PasswordEncoder
 import org.springframework.stereotype.Component
 
 @Component
-class CustomAuthenticationProvider(private val customUserDetailService: CustomUserDetailService) : AuthenticationProvider {
+class CustomAuthenticationProvider(private val customUserDetailService: CustomUserDetailService, private val passwordEncoder: PasswordEncoder) : AuthenticationProvider {
 
     override fun authenticate(authentication: Authentication): Authentication? {
         val id : String = authentication.name
         val password : String = authentication.credentials.toString()
         val userDetails : UserDetails = customUserDetailService.loadUserByUsername(id)
 
-        return null
+        if (!passwordEncoder.matches(password, userDetails.password)) throw BadCredentialsException("Bad Credentials")
+
+        return UsernamePasswordAuthenticationToken(
+            userDetails,
+            password,
+            userDetails.authorities
+        )
     }
 
     override fun supports(authentication: Class<*>?): Boolean {
